8634679ae22ca7151473b31136545a9eb43f73d6
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-12-07 23:11:50 +0900

    修正

--- src/components/TransactionItem.tsx
+++ src/components/TransactionItem.tsx
@@ -44,13 +44,13 @@ const TransactionItem: React.FC<TransactionProps> = ({
         />
         <span>{description}</span>
       </div>
-      <p className="min-w-[150px] items-center hidden md:flex">
+      <div className="min-w-[150px] items-center hidden md:flex">
         {category && (
           <div className="rounded-md text-xs bg-gray-700 dark:bg-gray-100 text-gray-100 dark:text-black px-2 py-0.5">
             {category}
           </div>
         )}
-      </p>
+      </div>
       <div className="min-w-[70px] text-right">{formattedAmount}</div>
       <div className="min-w-[50px] flex justify-end">...</div>
     </div>
