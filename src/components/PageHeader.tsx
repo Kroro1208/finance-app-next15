f084d743667c4ecd757b032b4bd88ecffe0296a6
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-12-07 19:48:45 +0900

    修正

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,20 +1,25 @@
-import type { NextPage } from "next"
-import Link from "next/link"
+import type { NextPage } from "next";
+import Link from "next/link";
 
 type ClassNameProps = {
-    className: string
-}
+  className: string;
+};
 
-const PageHeader:NextPage<ClassNameProps> = ({className}) => {
+const PageHeader: NextPage<ClassNameProps> = ({ className }) => {
   return (
     <header className={`flex justify-between items-center ${className}`}>
-      <Link href="/dashboard" className="text-xl hover:underline underline-offset-8 decoration-2">Finance App</Link>
+      <Link
+        href="/dashboard"
+        className="text-xl hover:underline underline-offset-8 decoration-2"
+      >
+        Finance App
+      </Link>
       <div className="flex items-center space-x-4">
         <div>切り替え</div>
         <div>ユーザープロフィール</div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default PageHeader
+export default PageHeader;
