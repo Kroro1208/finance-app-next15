1a012bf4f42086af8989c18c1f957d0b4296a29f
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-12-07 15:49:04 +0900

    初期設定

---
+++ src/app/playground/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PlayGround = () => {
+  return (
+    <main className='space-y-8'>
+      <h1 className='text-4xl mt-8'>Playground</h1>
+      <div>
+        <h2 className='mb-4 text-lg font-mono'>Component Name</h2>
+        <hr className='mb-4 border-gray-200 dark:border-gray-800'/>
+      </div>
+    </main>
+  )
+}
+
+export default PlayGround
